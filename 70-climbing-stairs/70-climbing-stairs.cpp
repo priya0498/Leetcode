3692332321a6b4ce45753f4d18889ab2b6802c46
Priya <73305161+priya0498@users.noreply.github.com>
2022-02-26 20:14:24 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.99%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+            return n;
+        int x = 1 , y = 2;
+        for( int i = 3 ; i < n ; i++)
+        {
+            int temp = y ;
+        y+= x;
+        x= temp;
+    }
+        return (x+y);
+    }
+};
