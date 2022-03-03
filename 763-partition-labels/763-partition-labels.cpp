e9956cc7c791a8473a82329126c02de3c51ae9c4
Priya <73305161+priya0498@users.noreply.github.com>
2022-03-03 08:50:27 +0530

    Time: 4 ms (74.23%), Space: 6.6 MB (63.77%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int> chr(26 , 0);
-       
-     //   int n = s.size();
-        for(int i = 0 ; i < s.size(); i++){
-            chr[s[i]-'a'] = i;
-            
-        }
-          vector<int> ans;
-            int last = 0 ;
-            int maxi = -1;
-           
-            for(int i = 0 ; i < s.size(); i++){
-                maxi = max(maxi , chr[s[i] - 'a']);
-                if(i== maxi){
-                    ans.push_back(maxi - last + 1);
-                last = i+1 ;
-                }
-            }
+        vector<int> lastindex(26 , 0);
+            for(int i = 0 ; i < s.size() ; i++){
+                lastindex[s[i] - 'a'] =i;
                 
-        
+            }
+        vector<int> ans;
+        int start = 0 , end = 0;
+        for(int i = 0 ; i < s.size() ; i++){
+            end = max(end , lastindex[s[i]-'a']);
+                if(i == end){
+                    ans.push_back(i - start + 1);
+                    start = i+1;
+                }
+        }
         return ans;
-        
     }
 };
