2ba38b6e2883416fcface923c013991b31ad3a65
Priya <73305161+priya0498@users.noreply.github.com>
2022-05-12 21:59:32 +0530

    Time: 18 ms (45.35%), Space: 19.1 MB (5.89%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -5,11 +5,11 @@ public:
         vector<int> ans;
         map<int , int> mp;
         for(int i = 0 ; i < nums.size() ; i++){
-            if(mp[nums[i]]==0)
-                ans.push_back(nums[i]);
+         if(mp[nums[i]]==0)
+         ans.push_back(nums[i]);
             mp[nums[i]]++;
         }
         nums = ans;
-            return ans.size();
+         return ans.size();
     }
 };
