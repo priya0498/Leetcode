111bda098936408a1a77279522eba33e9703889f
Priya <73305161+priya0498@users.noreply.github.com>
2022-05-19 11:23:27 +0530

    Time: 3 ms (87.03%), Space: 11.9 MB (93.43%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int k=-1;
-        int l;
+//         int n = nums.size();
+//         int k = -1;
+//         int l;
         
-        for (int i=0;i<n-1;i++){
-            if (nums[i]<nums[i+1]){
-                k=i;
-            }
-        }
-        if (k==-1){
-            sort(nums.begin(),nums.end());
-            return;
-        }
-        for (int i=k+1;i<n;i++){
-            if (nums[i]>nums[k]){
-                l=i;
-            }
-        }
+//         for (int i=0;i<n-1;i++){
+//             if (nums[i]<nums[i+1]){
+//                 k=i;
+//             }
+//         }
+//         if (k==-1){
+//             sort(nums.begin(),nums.end());
+//             return;
+//         }
+//         for (int i=k+1;i<n;i++){
+//             if (nums[i]>nums[k]){
+//                 l=i;
+//             }
+//         }
               
-        int tmp = nums[l];
-        nums[l]=nums[k];
-        nums[k]=tmp;
+//         int tmp = nums[l];
+//         nums[l]=nums[k];
+//         nums[k]=tmp;
         
-        reverse(nums.begin()+k+1,nums.end());
-    
+//         reverse(nums.begin()+k+1,nums.end());
+    next_permutation(nums.begin() , nums.end());
         
     }
 };
