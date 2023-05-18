3402ec759b341ce8b702192e1f37aeabf11329c1
Priya <73305161+priya0498@users.noreply.github.com>
2023-05-19 02:02:49 +0800

    Time: 234 ms (5.23%), Space: 6.9 MB (92.27%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -6,47 +6,46 @@ public:
              // }
              int n = str1.length();
              int m = str2.length();
-
+            
              int len = 1;
              int ans = -1;
-             while(len <= n) {
-                bool divides1 = true;
-                int j = 0;     
-                for(int i = 0; i < n; ++i) {
-                   if(str1[i] != str1[j]) {
-                        divides1 = false;
-                        break;    
-                   }
-                   j++;     
-                   j %= len;     
-                }
-                if(j != 0) {
-                        divides1 = false;
-                }     
-
-                bool divides2 = true;
-                j = 0;
-                for(int i = 0; i < m; ++i) {
-                    if(str2[i] != str1[j]) {
-                         divides2 = false;
-                         break;
+            
+            while(len <= n){
+          bool flag1 = true;
+                     int j = 0 ;
+            for(int i = 0 ; i < n ; i++){
+                    if(str1[i] != str1[j])
+                    {
+                            flag1 = false;
+                            break;                     
                     }
-                        j++;
-                    j %= len;    
-                }      
-                 if(j != 0) {
-                         divides2 = false;
-                 }       
-                if(divides2 == true && divides1 == true) {
-                    ans = len;    
-                }
-               cout << len << " " << divides1 << " " << divides2 <<endl;     
-len++;     
-             }                
-             if(ans == -1) {
-                return "";
-             }       
-             return str1.substr(0, ans);      
-      
+                    j++;
+                    j %= len;
+                    }
+            if(j!= 0){
+                    flag1 = false;
+            }
+            bool flag2 = true;
+             j = 0;
+            for(int i = 0 ; i < m ; i++){
+                    if(str2[i]!= str1[j]){
+                            flag2 = false;
+                            break;
+                    }
+                    j++;
+                    j %= len;
+            }
+            if( j!= 0){
+                    flag2 = false;
+            }
+            if(flag1 == true&& flag2==true){
+                     ans = len;
+            }
+            len++;
+            }
+            if(ans == -1){
+                    return "";
+            }
+            return str1.substr(0,ans);
     }
 };
