1d08dbc3257a1e87580063df398226593dce3281
Priya <73305161+priya0498@users.noreply.github.com>
2023-05-22 16:19:49 +0800

    Time: 9 ms (16.10%), Space: 8.9 MB (83.48%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-           vector<int> greater;
-         vector<bool> ans;
+         vector<int> greater;
             
+         vector<bool> ans;      
         for(int i = 0 ; i < candies.size(); i++){
                 
-        greater.push_back(candies[i] + extraCandies);
+         greater.push_back(candies[i] + extraCandies);
         }
             for(int i = 0 ; i < greater.size(); i++){
+                    
                     bool flag = true;
+                    
                     for(int j = 0  ; j < candies.size() ; j++){
+                            
                             if(greater[i]<candies[j]){
+                                    
                                     flag = false;
                                     break;
                             }
                             
                     }
-                    ans.push_back(flag);
-                   
+                    ans.push_back(flag);              
             }
             
             return ans;
