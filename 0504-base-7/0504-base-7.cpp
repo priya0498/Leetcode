5c9d3df7722bface76d0e1d09fa52590ce0a4f96
Priya <73305161+priya0498@users.noreply.github.com>
2023-05-31 11:58:56 +0800

    Time: 0 ms (100.00%), Space: 6 MB (60.37%) - LeetHub

--- 0504-base-7/0504-base-7.cpp
+++ 0504-base-7/0504-base-7.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     string convertToBase7(int num) {
-
-        
-        int s1 = 1 ;
-        if(num <=0 ){
-            if(num==0) return "0" ;
-            s1 = -1 ;
-            num = num*-1 ;
+    int s1 = 1;
+        if(num <=0){
+            if(num == 0){
+                
+                return "0";
+            }
+            s1 = -1;
+            num = num * -1;
         }
-        string ans = "" ;
-
-        while(num>0){
-
-            int rem = num%7 ;
-            ans  = to_string(rem) + ans ;
-            num = num/7 ;
+        string ans = "";
+        while(num > 0){
+           int  rem = num % 7;
+         
+            ans = to_string(rem) +  ans;
+           
+               num = num / 7;
         }
-
-        if(s1==-1){
-            ans = "-" + ans ;
+        if(s1 == -1){
+            ans = "-" + ans;
         }
-              return ans;
+        return ans;
     }
 };
