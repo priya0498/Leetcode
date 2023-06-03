7046272be8e6d908d1b23901a072bbf826e301dd
Priya <73305161+priya0498@users.noreply.github.com>
2023-06-03 16:25:17 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (59.70%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int convertTime(string current, string correct) {
+        
         int currentHours = 0;
         int currentMinute = 0;
         int correctHours = 0;
-        int correctMinute = 0;
+        int correctMinute = 0;   
         
         int i = 0;
+        
         while(current[i] != ':') {
             currentHours = currentHours * 10 + (current[i] - '0');
             ++i;
-        }
-        
+        }     
         ++i;
         while(i < current.size()) {
             currentMinute = currentMinute * 10 + (current[i] - '0');
@@ -32,10 +33,13 @@ public:
         currentMinute += currentHours * 60;
         correctMinute += correctHours * 60;
         vector<int> minutes = {60, 15, 5, 1};
+        
         int count = 0;
+        
         for(int i = 0; i < 4; ++i) {
-            int minute = minutes[i];
-                while(currentMinute < correctMinute) {
+            
+              int minute = minutes[i];
+            while(currentMinute < correctMinute) {
                 currentMinute += minute;
                 count++;
             }
